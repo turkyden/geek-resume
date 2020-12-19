cf1f29109a78c1aaa659a8ae6214d82894199a20
Turkyden <616287091@qq.com>
2020-12-20 02:21:26 +0800

    feat: add the us adn cn tabs in monaco editor

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,23 @@ import React, { useState, useRef } from 'react';
 import { render } from 'react-dom';
 import MonacoEditor from 'react-monaco-editor';
 import SplitPane from 'react-split-pane';
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdownWithHtml from 'react-markdown/with-html';
+import gfm from 'remark-gfm';
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 import './A4.css';
 import resume_cn from './resume_cn';
+import resume_us from './resume_us';
 import svg_cn from './cn.svg';
 import svg_us from './us.svg'
 
 function App() {
+  const [zNMode, setZNMode] = useState(true);
 
-  const [code, setCode] = useState(resume_cn);
+  const [resumeCN, setResumeCN] = useState(resume_cn);
+
+  const [resumeUS, setResumeUS] = useState(resume_us);
+
+  const code = zNMode ? resumeCN : resumeUS;
   
   const [darkMode, setDarkMode] = useState(true);
 
@@ -22,9 +29,14 @@ function App() {
   const editorDidMount = (editor: monaco.editor.IStandaloneCodeEditor): void => {
     myEditor.current = editor;
     myEditor.current.focus();
+    myEditor.current.updateOptions({
+      minimap: {
+        enabled: false
+      }
+    })
   }
 
-  const onMonacoChange = (newValue: any) => setCode(newValue);
+  const onMonacoChange = (newValue: any) => zNMode ? setResumeCN(newValue) : setResumeUS(newValue);
 
   const onSplitChange = (size: unknown) => {
     const newSize = size as number[];
@@ -37,31 +49,44 @@ function App() {
     <div className="w-screen h-screen">
       <SplitPane split="vertical" onChange={onSplitChange} >
         <div className="w-full h-full bg-gray-100">
-          <div className="w-full bg-black h-12 flex justify-center items-center px-4">
-            <a className="text-white text-lg" href="https://www.baidu.com" target="_blank" rel="noreferrer">Geek Resume</a>
+          <div className="w-full bg-purple-700 h-12 flex justify-center items-center px-4">
+            <a className="text-white text-lg no-underline" href="https://www.baidu.com" target="_blank" rel="noreferrer">Geek Resume</a>
           </div>
           <div className="flex justify-center overflow-scroll" style={{ height: 'calc(100% - 48px)' }}>
-            <ReactMarkdown className="A4 markdown-body">{code}</ReactMarkdown>
+            <div className="A4Wrapper">
+              <ReactMarkdownWithHtml plugins={[gfm]} allowDangerousHtml className="A4 markdown-body" >
+                {code}
+              </ReactMarkdownWithHtml>
+            </div>
           </div>
         </div>
         <div className="w-full h-full">
-          <div className="w-full bg-black h-12 flex justify-between items-center px-4">
-            <ul className="flex items-center text-white h-full">
-              <li className="px-4 h-full flex items-center cursor-pointer border-0 border-b-2 border-green-300">
-                <img className="w-6" src={svg_cn} alt="zh-cn" />
+          <div className="w-full bg-purple-700 h-12 flex justify-between items-center px-4">
+            <ul className="flex items-center text-white h-full list-none">
+              <li 
+                className={`px-2 mx-2 h-full flex items-center cursor-pointer border-solid border-0 border-b-2 ${ zNMode ? 'border-green-300' : 'border-transparent' }`} 
+                onClick={() => setZNMode(true)}
+              >
+                <img className="w-4" src={svg_cn} alt="zh-cn" />
                 <span className="pl-4 text-sm">中文</span>
               </li>
-              <li className="px-4 h-full flex items-center cursor-pointer border-0 border-green-300">
-                <img className="w-6" src={svg_us} alt="us" />
+              <li 
+                className={`px-2 mx-2 h-full flex items-center cursor-pointer border-solid border-0 border-b-2 ${ !zNMode ? 'border-green-300' : 'border-transparent' }`}
+                onClick={() => setZNMode(false)}
+              >
+                <img className="w-4" src={svg_us} alt="us" />
                 <span className="pl-4 text-sm">English</span>
               </li>
             </ul>
-            <ul className="w-64 flex justify-between">
-              <li className="">linkin / Boss / </li>
-              <li className="">Export as PDF / PNG / Markdown</li>
+            <ul className="flex justify-between list-none">
+              {/* <li className="">linkin / Boss / </li>
+              <li className="">Export as PDF / PNG / Markdown</li> */}
+              <li className="cursor-pointer text-white pr-4" onClick={() => window.print()}>
+                <span>🖨️</span>
+              </li>
               <li className="cursor-pointer flex items-center" onClick={() => setDarkMode(value => !value)}>
                 <span>{ darkMode ? '🌕' : '🌙' }</span>
-              </li>e
+              </li>
             </ul>
           </div>
           <MonacoEditor
--- src/resume_cn.ts
+++ src/resume_cn.ts
@@ -1,25 +1,61 @@
 const RESUME_CN = `
-# Hello, *world*!
+# Your Name
 
-- ddd
-- cfvvv
-    - ddf
-    - fdsfsfdsfd
-        - ccddd
+<img src="https://avatars0.githubusercontent.com/u/24560160?s=460&u=36a6072b8220e6ad7c0c7f7dbf97cc3dd796a8d0&v=4" width="160" height="160" alt="avatar" align="right">
 
-1. ddd
-2. dddd
+[![GitHub User's stars](https://img.shields.io/github/stars/turkyden?style=social)](https://github.com/Turkyden)
+[![Github](https://img.shields.io/github/followers/Turkyden?label=Follow&style=social)](https://github.com/Turkyden)
+[![Linkedin](https://img.shields.io/badge/-Dengju%20Deng-blue?style=flat-square&logo=linkedin&logoColor=white&link=https://www.linkedin.com/in/dengju-deng-8707b7a5/)](https://www.linkedin.com/in/alexey-khachatryan-8707b7a5/)
+[![Mail](https://img.shields.io/badge/-wj871287@gmail.com-gray?style=flat-square&logo=gmail&logoColor=red&link=)](mailto:wj871287@gmail.com)
 
-> cddsdsds
-> 
-> dsdsdsdsdsdsds
-> dsdsdsdsdsdsds
-> > dsdsdsdsdsds
+I'm a software development engineer. I like to work with React and Typescript. I love the user experience design and technology. I'm open-source development enthusiast.
+
+**Workflow and Skill Stack I work with**
+
+<code><img height="20" alt="github" src="https://camo.githubusercontent.com/b079fe922f00c4b86f1b724fbc2e8141c468794ce8adbc9b7456e5e1ad09c622/68747470733a2f2f6564656e742e6769746875622e696f2f537570657254696e7949636f6e732f696d616765732f7376672f6769746875622e737667"></code>
+<code><img height="20" alt="dribbble" src="https://camo.githubusercontent.com/ad020ac08fdbf9becdb52a0d5e18ec3eff97c297d8c7ef9f6ff06459e39fa984/68747470733a2f2f6564656e742e6769746875622e696f2f537570657254696e7949636f6e732f696d616765732f7376672f6472696262626c652e737667"></code>
+<code><img height="20" alt="chrome" src="https://camo.githubusercontent.com/9f9d124d411068111c0c4707b245a3461c5c1171f7310b802c1be1353c68c93d/68747470733a2f2f6564656e742e6769746875622e696f2f537570657254696e7949636f6e732f696d616765732f7376672f6368726f6d652e737667"></code>
+<code>
+  <a title="知乎" href="https://www.zhihu.com/people/a-ju-76" target="_blank">
+    <img height="20" alt="zhihu" src="https://static.zhihu.com/static/favicon.ico">
+  </a>
+</code>
+<code>
+  <a title="掘金" href="https://juejin.im/user/1398234519441261" target="_blank">
+    <img height="20" alt="juejin" src="https://b-gold-cdn.xitu.io/favicons/v2/favicon-32x32.png">
+  </a>
+</code>
+<code>
+  <a title="stackoverflow" href="https://stackoverflow.com/users/9764081/turkyden" target="_blank">
+    <img height="20" alt="segmentfault" src="https://cdn.sstatic.net/Sites/stackoverflow/Img/favicon.ico?v=ec617d715196">
+  </a>
+</code>
+<code>
+  <a title="codesandbox" href="https://codesandbox.io/u/Turkyden" target="_blank">
+    <img height="20" alt="juejin" src="https://codesandbox.io/favicon.ico">
+  </a>
+</code>
+<code>
+  <a title="codepen" href="https://codepen.io/Turkyden" target="_blank">
+    <img height="20" alt="juejin" src="https://static.codepen.io/assets/favicon/favicon-aec34940fbc1a6e787974dcd360f2c6b63348d4b1f4e06c77743096d55480f33.ico">
+  </a>
+</code>
+
+<code><img height="50" src="https://www.vectorlogo.zone/logos/reactjs/reactjs-ar21.svg"></code>
+<code><img height="50" src="https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-ar21.svg"></code>
+<code><img height="50" src="https://www.vectorlogo.zone/logos/graphql/graphql-ar21.svg"></code>
+<code><img height="50" src="https://www.vectorlogo.zone/logos/nestjs/nestjs-ar21.svg"></code>
+<code><img height="50" src="https://www.vectorlogo.zone/logos/electronjs/electronjs-ar21.svg"></code>
+<code><img height="50" src="https://www.vectorlogo.zone/logos/nodejs/nodejs-ar21.svg"></code>
 
 
-| sss | fvvcvc |
-| ---- | ---- |
-| xsdsds | kcldfkld |
+<!--
+
+## Interesting stats
+
+[![Anurag's github stats](https://github-readme-stats.vercel.app/api?username=Turkyden)](https://github.com/Turkyden)
+
+-->
 `
 
 export default RESUME_CN
