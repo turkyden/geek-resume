db6d05cb1e4db15bc8d4e8c8a5adb4ae7c7b3aae
Turkyden <616287091@qq.com>
2020-12-20 22:33:39 +0800

    fix: fix the layout style and add the github corner

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useRef } from 'react';
 import { render } from 'react-dom';
 import MonacoEditor from 'react-monaco-editor';
 import SplitPane from 'react-split-pane';
+import GithubCorner from 'react-github-corner';
 import ReactMarkdownWithHtml from 'react-markdown/with-html';
 import gfm from 'remark-gfm';
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
@@ -20,7 +21,7 @@ function App() {
 
   const code = zNMode ? resumeCN : resumeUS;
   
-  const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(false);
 
   const myEditor = useRef<monaco.editor.IStandaloneCodeEditor>();
 
@@ -48,13 +49,10 @@ function App() {
   return (
     <div className="w-screen h-screen">
       <SplitPane split="vertical" onChange={onSplitChange} >
-        <div className="w-full h-full bg-gray-100">
-          <div className="w-full bg-purple-700 h-12 flex justify-center items-center px-4">
-            <a className="text-white text-lg no-underline" href="https://www.baidu.com" target="_blank" rel="noreferrer">Geek Resume</a>
-          </div>
-          <div className="flex justify-center overflow-scroll" style={{ height: 'calc(100% - 48px)' }}>
+        <div className="w-full h-full">
+          <div className="flex justify-center overflow-scroll" style={{ height: '100%', backgroundColor: darkMode ? '#1e1e1e' : '#ffffff' }}>
             <div className="A4Wrapper">
-              <ReactMarkdownWithHtml plugins={[gfm]} allowDangerousHtml className="A4 markdown-body" >
+              <ReactMarkdownWithHtml plugins={[gfm]} allowDangerousHtml className="A4 markdown-body shadow-2xl" >
                 {code}
               </ReactMarkdownWithHtml>
             </div>
@@ -63,15 +61,18 @@ function App() {
         <div className="w-full h-full">
           <div className="w-full bg-purple-700 h-12 flex justify-between items-center px-4">
             <ul className="flex items-center text-white h-full list-none">
+              <li className="pr-10">
+                <a className="text-white text-lg no-underline font-mono" href="https://github.com/Turkyden/geek-resume" target="_blank" rel="noreferrer">Geek Resume</a>
+              </li>
               <li 
-                className={`px-2 mx-2 h-full flex items-center cursor-pointer border-solid border-0 border-b-2 ${ zNMode ? 'border-green-300' : 'border-transparent' }`} 
+                className={`px-4 h-full flex items-center cursor-pointer ${ zNMode ? 'bg-purple-800' : 'bg-transparent' }`} 
                 onClick={() => setZNMode(true)}
               >
                 <img className="w-4" src={svg_cn} alt="zh-cn" />
                 <span className="pl-4 text-sm">中文</span>
               </li>
               <li 
-                className={`px-2 mx-2 h-full flex items-center cursor-pointer border-solid border-0 border-b-2 ${ !zNMode ? 'border-green-300' : 'border-transparent' }`}
+                className={`px-4 h-full flex items-center cursor-pointer ${ !zNMode ? 'bg-purple-800' : 'bg-transparent' }`}
                 onClick={() => setZNMode(false)}
               >
                 <img className="w-4" src={svg_us} alt="us" />
@@ -81,10 +82,10 @@ function App() {
             <ul className="flex justify-between list-none">
               {/* <li className="">linkin / Boss / </li>
               <li className="">Export as PDF / PNG / Markdown</li> */}
-              <li className="cursor-pointer text-white pr-4" onClick={() => window.print()}>
+              <li className="cursor-pointer text-white pr-4 transition duration-300 ease-in-out transform hover:scale-125" onClick={() => window.print()}>
                 <span>🖨️</span>
               </li>
-              <li className="cursor-pointer flex items-center" onClick={() => setDarkMode(value => !value)}>
+              <li className="cursor-pointer flex items-center transition duration-300 ease-in-out transform hover:scale-125" onClick={() => setDarkMode(value => !value)}>
                 <span>{ darkMode ? '🌕' : '🌙' }</span>
               </li>
             </ul>
@@ -101,6 +102,13 @@ function App() {
           />
         </div>
       </SplitPane>
+      <GithubCorner
+        href="https://github.com/Turkyden/geek-resume"
+        bannerColor="#6D28D9"
+        octoColor="#fff"
+        size={80}
+        direction="left" 
+      />
     </div>
   );
 }
