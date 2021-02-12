78f724e6dc4b11845bba8ac7d3256a902bf3c185
Dengju Deng <wj871287@gmail.com>
2021-02-12 11:08:11 +0800

    Update resume_cn.ts

--- src/resume_cn.ts
+++ src/resume_cn.ts
@@ -1,5 +1,6 @@
 const RESUME_CN = `
-# Your Name
+
+# 前端开发工程师
 
 <img src="https://avatars0.githubusercontent.com/u/24560160?s=460&u=36a6072b8220e6ad7c0c7f7dbf97cc3dd796a8d0&v=4" width="160" height="160" alt="avatar" align="right">
 
@@ -8,7 +9,8 @@ const RESUME_CN = `
 [![Linkedin](https://img.shields.io/badge/-Dengju%20Deng-blue?style=flat-square&logo=linkedin&logoColor=white&link=https://www.linkedin.com/in/dengju-deng-8707b7a5/)](https://www.linkedin.com/in/alexey-khachatryan-8707b7a5/)
 [![Mail](https://img.shields.io/badge/-wj871287@gmail.com-gray?style=flat-square&logo=gmail&logoColor=red&link=)](mailto:wj871287@gmail.com)
 
-I'm a software development engineer. I like to work with React and Typescript. I love the user experience design and technology. I'm open-source development enthusiast.
+> 你好，我是**邓登举**，一个专注于人机交互技术的 Web 开发者，[alibaba/hooks](https://github.com/alibaba/hooks) 核心维护者之一，[退役军人]()，开源爱好者。
+
 <!-- My social links -->
 <code><img height="20" alt="github" src="https://camo.githubusercontent.com/b079fe922f00c4b86f1b724fbc2e8141c468794ce8adbc9b7456e5e1ad09c622/68747470733a2f2f6564656e742e6769746875622e696f2f537570657254696e7949636f6e732f696d616765732f7376672f6769746875622e737667"></code>
 <code><img height="20" alt="dribbble" src="https://camo.githubusercontent.com/ad020ac08fdbf9becdb52a0d5e18ec3eff97c297d8c7ef9f6ff06459e39fa984/68747470733a2f2f6564656e742e6769746875622e696f2f537570657254696e7949636f6e732f696d616765732f7376672f6472696262626c652e737667"></code>
@@ -16,9 +18,57 @@ I'm a software development engineer. I like to work with React and Typescript. I
 <code><img height="20" alt="zhihu" src="https://static.zhihu.com/static/favicon.ico"></code>
 <code><img height="20" alt="juejin" src="https://b-gold-cdn.xitu.io/favicons/v2/favicon-32x32.png"></code>
 <code><img height="20" alt="segmentfault" src="https://cdn.sstatic.net/Sites/stackoverflow/Img/favicon.ico?v=ec617d715196"></code>
+<code><img height="20" alt="segmentfault" src="https://cdn.sstatic.net/Sites/stackoverflow/Img/favicon.ico?v=ec617d715196"></code>
+<code><img height="20" alt="segmentfault" src="https://cdn.sstatic.net/Sites/stackoverflow/Img/favicon.ico?v=ec617d715196"></code>
+<code><img height="20" alt="segmentfault" src="https://cdn.sstatic.net/Sites/stackoverflow/Img/favicon.ico?v=ec617d715196"></code>
+<code><img height="20" alt="segmentfault" src="https://cdn.sstatic.net/Sites/stackoverflow/Img/favicon.ico?v=ec617d715196"></code>
+<code><img height="20" alt="segmentfault" src="https://cdn.sstatic.net/Sites/stackoverflow/Img/favicon.ico?v=ec617d715196"></code>
+<code><img height="20" alt="segmentfault" src="https://cdn.sstatic.net/Sites/stackoverflow/Img/favicon.ico?v=ec617d715196"></code>
+
+<code><img height="40" src="https://www.vectorlogo.zone/logos/reactjs/reactjs-ar21.svg"></code>
+<code><img height="40" src="https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-ar21.svg"></code>
+<code><img height="40" src="https://www.vectorlogo.zone/logos/nestjs/nestjs-ar21.svg"></code>
+<code><img height="40" src="https://www.vectorlogo.zone/logos/electronjs/electronjs-ar21.svg"></code>
+<code><img height="40" src="https://www.vectorlogo.zone/logos/nodejs/nodejs-ar21.svg"></code>
+
+## 教育经历
+
+2018.07 ~ 2021.07 同济大学 艺术设计专业硕士（非全日制）
+
+主修中外设计史，平面设计，应用交互设计，在职学习中
+
+2013.09 ~ 2017.07 上海第二工业大学 电子商务应用技术 工学学士（全日制）
+
+主修计算机网络通识课程，J2EE 以及 Java Web 开发，有基本的服务端编程经验
+
+2011.09 ~ 2013.09 中国人民解放军陆军第 31 集团军
+
+大学生国防服役计划，为期 2 年的基层部队服役生涯，培养了我严谨认真和团队协作的品格
+
+## 工作履历
+
+2018.09 ~ 至今 泛微 OA（主板上市）/ 产品方案部 / 开发工程师 
+
+行业：OA 办公自动化 / SaaS 企业级协同办公软件
+
+主要负责云平台，以及前端可视化编辑器开发的核心业务。
+
+一）SPA 单页应用 Saas 产品前端可二开方案的落地与 JS SDK 开发
+
+二）Low Code 低代码门户搭建素材
+
+2017.07 ~ 2018.09 联和金融 / 金融科技部 / 开发工程师
+
+行业：科技金融 / 区块链
+
+主要负责 React 组件开发基础
+
+## 
+
+<!--
 <code><img height="20" alt="juejin" src="https://codesandbox.io/favicon.ico"></code>
 <code><img height="20" alt="juejin" src="https://static.codepen.io/assets/favicon/favicon-aec34940fbc1a6e787974dcd360f2c6b63348d4b1f4e06c77743096d55480f33.ico"></code>
-<!-- Tech stack i work with -->
+<!-- Tech stack i work with 
 <code><img height="50" src="https://www.vectorlogo.zone/logos/reactjs/reactjs-ar21.svg"></code>
 <code><img height="50" src="https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-ar21.svg"></code>
 <code><img height="50" src="https://www.vectorlogo.zone/logos/nestjs/nestjs-ar21.svg"></code>
@@ -26,6 +76,7 @@ I'm a software development engineer. I like to work with React and Typescript. I
 <code><img height="50" src="https://www.vectorlogo.zone/logos/nodejs/nodejs-ar21.svg"></code>
 
 ![Anurag's github stats](https://github-readme-stats.vercel.app/api?username=Turkyden)
+ -->
 `
 
 export default RESUME_CN
