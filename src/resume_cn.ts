be768b9f670b7f3dbc0e4f369bed8af0a0a639c8
Turkyden <wj871287@gmail.com>
2021-05-05 12:29:54 +0800

    docs: ✏️ fixed the resume cn-zh

--- src/resume_cn.ts
+++ src/resume_cn.ts
@@ -6,12 +6,12 @@ const RESUME_CN = `
  <span style="display: none; position: absolute; bottom: -160px; right: 0; background: #fff; border-radius: 100%; padding: 8px; font-size: 12px">{ ...🧡 }</span>
 </div>
 
-[![GitHub User's stars](https://img.shields.io/github/stars/turkyden?style=social)](https://github.com/Turkyden)
+[![GitHub User's stars](https://img.shields.io/github/stars/alibaba/hooks?style=social)](https://github.com/Turkyden)
 [![Github](https://img.shields.io/github/followers/Turkyden?label=Follow&style=social)](https://github.com/Turkyden)
-[![Linkedin](https://img.shields.io/badge/-Dengju%20Deng-blue?style=flat-square&logo=linkedin&logoColor=white&link=https://www.linkedin.com/in/dengju-deng-8707b7a5/)](https://www.linkedin.com/in/alexey-khachatryan-8707b7a5/)
 [![Mail](https://img.shields.io/badge/-wj871287@gmail.com-gray?style=flat-square&logo=gmail&logoColor=red&link=)](mailto:wj871287@gmail.com)
+[![Wechat](https://img.shields.io/badge/-15026579658-07c160?style=flat-square&logo=Wechat&logoColor=white&link=https://www.linkedin.com/in/dengju-deng-8707b7a5/)](https://www.linkedin.com/in/alexey-khachatryan-8707b7a5/)
 
-你好，我是邓登举，一个专注于人机交互与体验技术的 React 开发者，[alibaba/hooks](https://github.com/alibaba/hooks) 核心维护者，开源爱好者，退役军人。
+你好，我是邓登举，一个专注于人机交互与体验技术的 React 开发者，擅长于低代码可视化搭建平台解决方案，Github 开源爱好者，退役军人。
 
 <!-- My social links -->
 <code><img height="20" alt="github" src="https://camo.githubusercontent.com/b079fe922f00c4b86f1b724fbc2e8141c468794ce8adbc9b7456e5e1ad09c622/68747470733a2f2f6564656e742e6769746875622e696f2f537570657254696e7949636f6e732f696d616765732f7376672f6769746875622e737667"></code>
@@ -32,33 +32,33 @@ const RESUME_CN = `
 
 ### 教育经历
 
-<code>2018.07 ~ 2021.07</code> 同济大学 / 设计硕士 / 非全日制
+**<code>2018.07 ~ 2021.07</code>** 同济大学 / 设计硕士 / 非全日制
 
 主修中外设计史，平面设计，应用交互设计，在职学习中
 
-<code>2011.09 ~ 2017.07</code> 上海第二工业大学 / 电子商务应用技术 / 工学学士 / 全日制
+**<code>2013.09 ~ 2017.07</code>** 上海第二工业大学 / 电子商务应用技术 / 工学学士 / 全日制
 
 主修计算机网络通识课程以及 J2EE 等电子商务开发技术，了解 Java Web 服务端编程
 
 ### 工作履历
 
-<code>2018.09 ~ 2021.03</code> 上海泛微网络股份有限公司 / 主板上市 / 开发工程师 
+**<code>2018.09 ~ 2021.05</code>** 上海泛微网络股份有限公司 / 主板上市 / 资深开发工程师 
 
-任职期间主要负责企业级 OA 协同办公软件 SaaS 云平台的建设，以及可视化编辑平台的研发。
+任职期间主要负责：OA 协同办公软件产品研发，企业级中后台低代码物料中心的建设。
 
-**ECode 无侵入二次开发框架**：为了解决 SaaS 产品交付实施成本过高与客户难以定制化的问题，我提出使用 React.forward 组件代理的方式改造标准 UI 组件，并编写 MiddleWare SDK，赋能 SaaS 标准产品的无侵入二次开发，为后续的可视化编辑的落地打下基础。作为核心成员，我还负责该框架在企业内部的推广和技术培训，最终根据交付实施部门统计，定制化二次开发的成本下降了 40% 左右。
+**ECode 无侵入二次开发框架**
 
-**EDesign 低代码开发平台**：是我参与的复杂度较高的项目之一，Low Code Platform 是今年公司主推的 SaaS 产品新特性，目的是使得软件产品交付使用之后，客户可以根据实际的需求搭建和修改前端页面，通过牺牲代码的灵活性来换取垂直业务领域的研发效能提升。我主要负责自定义 React Hooks 的开发与可视化配置面板 UI 组件的设计。（React + Mobx + Typescript + Webpack）
+为了解决 SaaS 产品交付实施成本过高与客户难以定制化的问题，我提出使用 React.forward 组件代理的方式改造标准 UI 组件，并编写 MiddleWare SDK，赋能 SaaS 标准产品的无侵入二次开发，为后续的可视化编辑的落地打下基础。作为核心成员，我还负责该框架在企业内部的推广和技术培训，最终根据交付实施部门统计，定制化二次开发的成本下降了 40% 左右。
 
-<code>2017.07 ~ 2018.09</code> 联和金融集团有限公司 / 开发工程师
+**EDesign 低代码开发平台**
 
-**Hey UI 组件库**：任职期间主要负责 React UI 组件库的开发与文档维护工作，因为在初级版本的 AntD v1.0 中无法实现黑夜模式，而且一些特殊的 UI 组件无法支撑 CNABS 资产证券化中后台业务的 UI 设计需求，我基于 Stroybook 重新封装 rc-components 实现了一套平行于 AntD 的企业级组件库。不仅满足了团队业务开发的需要，而且还使得公司的设计资产能够以技术的方式沉淀下来。
+是我参与的复杂度较高的项目之一，Low Code Platform 是今年公司主推的 SaaS 产品新特性，目的是使得软件产品交付使用之后，客户可以根据实际的需求搭建和修改前端页面，通过牺牲代码的灵活性来换取垂直业务领域的研发效能提升。我主要负责自定义 React Hooks 的开发与可视化配置面板 UI 组件的设计。（React + Mobx + Typescript + Webpack）
 
-<code>2013.07 ~ 2015.09</code> 中国人民解放军陆军第 31 集团军 / 副班长兼文书
+**<code>2017.07 ~ 2018.09</code>** 联和金融集团有限公司 / 开发工程师
 
-任职期间除了军事训练，兼任师部信管文书，负责集团军官网简单开发与 CMS 维护，展示军队风采。
+任职期间主要负责 Hey UI 组件库的开发与文档维护工作，因为在初级版本的 AntD v1.0 中无法实现黑夜模式，而且一些特殊的 UI 组件无法支撑 CNABS 资产证券化中后台业务的 UI 设计需求，我基于 Stroybook 重新封装 rc-components 实现了一套平行于 AntD 的企业级组件库。不仅满足了团队业务开发的需要，而且还使得公司的设计资产能够以技术的方式沉淀下来。
 
-<br/>
+<!-- <br/>
 
 ### 开源项目
 
@@ -94,7 +94,7 @@ const RESUME_CN = `
   <a href="https://github.com/anuraghazra/github-readme-stats">
     <img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username=anuraghazra&repo=github-readme-stats&hide_border=true" />
   </a>
-</div>
+</div> -->
 `
 
 export default RESUME_CN
