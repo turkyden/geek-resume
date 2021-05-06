726704c93982ee5a08b1c4015ee1cd7d4422c4b7
Turkyden <wj871287@gmail.com>
2021-05-07 07:59:59 +0800

    docs: ✏️ fixed the docs

--- src/resume_cn.ts
+++ src/resume_cn.ts
@@ -14,15 +14,16 @@ const RESUME_CN = `
 你好，我是邓登举，一个专注于人机交互与体验技术的 React 开发者，擅长于低代码可视化搭建平台解决方案，Github 开源爱好者，退役军人。
 
 <!-- My social links -->
-<code><img height="20" alt="github" src="https://camo.githubusercontent.com/b079fe922f00c4b86f1b724fbc2e8141c468794ce8adbc9b7456e5e1ad09c622/68747470733a2f2f6564656e742e6769746875622e696f2f537570657254696e7949636f6e732f696d616765732f7376672f6769746875622e737667"></code>
-<code><img height="20" alt="chrome" src="https://camo.githubusercontent.com/9f9d124d411068111c0c4707b245a3461c5c1171f7310b802c1be1353c68c93d/68747470733a2f2f6564656e742e6769746875622e696f2f537570657254696e7949636f6e732f696d616765732f7376672f6368726f6d652e737667"></code>
-<code><img height="20" alt="zhihu" src="https://static.zhihu.com/static/favicon.ico"></code>
-<code><img height="20" alt="segmentfault" src="https://camo.githubusercontent.com/008d0c2d87e4310eda2c92e711b4dd3df8818f0e65a219c8329a935fef40a2f1/68747470733a2f2f63646e2e7365676d656e746661756c742e636f6d2f762d35666237376638342f676c6f62616c2f696d672f66617669636f6e2e69636f"></code>
-<code><img height="20" alt="juejin" src="https://b-gold-cdn.xitu.io/favicons/v2/favicon-32x32.png"></code>
-<code><img height="20" alt="stackoverflow" src="https://cdn.sstatic.net/Sites/stackoverflow/Img/favicon.ico?v=ec617d715196"></code>
-<code><img height="20" alt="codepen" src="https://camo.githubusercontent.com/8370aa5746ed07f4c5b9d4b1fb1930924f1de40475a29047580a0be8e571b599/68747470733a2f2f7374617469632e636f646570656e2e696f2f6173736574732f66617669636f6e2f66617669636f6e2d616563333439343066626331613665373837393734646364333630663263366236333334386434623166346530366337373734333039366435353438306633332e69636f"></code>
-<code><img height="20" alt="codesandbox" src="https://camo.githubusercontent.com/1456820f3a6a3b8152a5c958ecbcf25e5eb2ef2034c566a101bacd41bf8ca2ee/68747470733a2f2f636f646573616e64626f782e696f2f66617669636f6e2e69636f"></code>
-<code><img height="20" alt="dribbble" src="https://camo.githubusercontent.com/ad020ac08fdbf9becdb52a0d5e18ec3eff97c297d8c7ef9f6ff06459e39fa984/68747470733a2f2f6564656e742e6769746875622e696f2f537570657254696e7949636f6e732f696d616765732f7376672f6472696262626c652e737667"></code>
+<code><a href="https://github.com/Turkyden" target="_blank"><img height="20" alt="github" src="https://camo.githubusercontent.com/b079fe922f00c4b86f1b724fbc2e8141c468794ce8adbc9b7456e5e1ad09c622/68747470733a2f2f6564656e742e6769746875622e696f2f537570657254696e7949636f6e732f696d616765732f7376672f6769746875622e737667"></a></code>
+<code><a href="https://www.google.com/chrome/" target="_blank"><img height="20" alt="chrome" src="https://camo.githubusercontent.com/9f9d124d411068111c0c4707b245a3461c5c1171f7310b802c1be1353c68c93d/68747470733a2f2f6564656e742e6769746875622e696f2f537570657254696e7949636f6e732f696d616765732f7376672f6368726f6d652e737667"></a></code>
+<code><a href="https://www.zhihu.com/people/a-ju-76" target="_blank"><img height="20" alt="zhihu" src="https://static.zhihu.com/static/favicon.ico"></a></code>
+<code><a href="https://segmentfault.com/u/turkyden" target="_blank"><img height="20" alt="segmentfault" src="https://camo.githubusercontent.com/008d0c2d87e4310eda2c92e711b4dd3df8818f0e65a219c8329a935fef40a2f1/68747470733a2f2f63646e2e7365676d656e746661756c742e636f6d2f762d35666237376638342f676c6f62616c2f696d672f66617669636f6e2e69636f"></a></code>
+<code><a href="https://www.behance.net/Turkyden" target="_blank"><img height="20" alt="behance" src="https://a5.behance.net/d08c757ea5f13023ff0d658534fe01d7fcec6097/img/site/favicon.ico?cb=264615658"></a></code>
+<code><a href="https://juejin.cn/user/1099167357484327" target="_blank"><img height="20" alt="juejin" src="https://b-gold-cdn.xitu.io/favicons/v2/favicon-32x32.png"></a></code>
+<code><a href="https://stackoverflow.com/" target="_blank"><img height="20" alt="stackoverflow" src="https://cdn.sstatic.net/Sites/stackoverflow/Img/favicon.ico?v=ec617d715196"></a></code>
+<code><a href="https://codepen.io/turkyden" target="_blank"><img height="20" alt="codepen" src="https://camo.githubusercontent.com/8370aa5746ed07f4c5b9d4b1fb1930924f1de40475a29047580a0be8e571b599/68747470733a2f2f7374617469632e636f646570656e2e696f2f6173736574732f66617669636f6e2f66617669636f6e2d616563333439343066626331613665373837393734646364333630663263366236333334386434623166346530366337373734333039366435353438306633332e69636f"></a></code>
+<code><a href="https://codesandbox.io/u/Turkyden" target="_blank"><img height="20" alt="codesandbox" src="https://camo.githubusercontent.com/1456820f3a6a3b8152a5c958ecbcf25e5eb2ef2034c566a101bacd41bf8ca2ee/68747470733a2f2f636f646573616e64626f782e696f2f66617669636f6e2e69636f"></a></code>
+<code><a href="https://dribbble.com/Turkyden" target="_blank"><img height="20" alt="dribbble" src="https://camo.githubusercontent.com/ad020ac08fdbf9becdb52a0d5e18ec3eff97c297d8c7ef9f6ff06459e39fa984/68747470733a2f2f6564656e742e6769746875622e696f2f537570657254696e7949636f6e732f696d616765732f7376672f6472696262626c652e737667"></a></code>
 
 <code><img height="40" src="https://www.vectorlogo.zone/logos/reactjs/reactjs-ar21.svg"></code>
 <code><img height="40" src="https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-ar21.svg"></code>
@@ -34,9 +35,9 @@ const RESUME_CN = `
 
 **<code>2018.07 ~ 2021.07</code>** 同济大学 / 设计硕士 / 非全日制
 
-主修中外设计史，平面设计，应用交互设计，在职学习中
+主修中外设计史，数字媒体艺术，平面设计、应用交互设计理论等
 
-**<code>2013.09 ~ 2017.07</code>** 上海第二工业大学 / 电子商务应用技术 / 工学学士 / 全日制
+**<code>2011.09 ~ 2017.07</code>** 上海第二工业大学 / 电子商务应用技术 / 工学学士 / 全日制
 
 主修计算机网络通识课程以及 J2EE 等电子商务开发技术，了解 Java Web 服务端编程
 
@@ -44,19 +45,19 @@ const RESUME_CN = `
 
 **<code>2018.09 ~ 2021.05</code>** 上海泛微网络股份有限公司 / 主板上市 / 资深开发工程师 
 
-任职期间主要负责：OA 协同办公软件产品研发，企业级中后台低代码物料中心的建设。
+任职期间主要负责：OA 协同办公软件产品研发，企业级中后台低代码物料素材中心的建设。
 
-**ECode 无侵入二次开发框架**
+**WeCode 无侵入二次开发框架**
 
-为了解决 SaaS 产品交付实施成本过高与客户难以定制化的问题，我提出使用 React.forward 组件代理的方式改造标准 UI 组件，并编写 MiddleWare SDK，赋能 SaaS 标准产品的无侵入二次开发，为后续的可视化编辑的落地打下基础。作为核心成员，我还负责该框架在企业内部的推广和技术培训，最终根据交付实施部门统计，定制化二次开发的成本下降了 40% 左右。
+针对企业数字化转型 SaaS 产品交付实施中，二开成本过高与客户定制化难的痛点，我提出一套企业级中后台应用无侵入二次开发解决方案。我主要负责二开平台的前端 IDE 架构、SDK 封装与方案落地工作，后期负责云平台的二开案例市场和内部技术培训。最终根据交付实施部门统计，定制化二次开发的成本下降 40% 左右，约合 1000 人/天。(技术栈：React + Mobx + Monaco Editor)
 
-**EDesign 低代码开发平台**
+**WeDesign 低代码开发平台**
 
-是我参与的复杂度较高的项目之一，Low Code Platform 是今年公司主推的 SaaS 产品新特性，目的是使得软件产品交付使用之后，客户可以根据实际的需求搭建和修改前端页面，通过牺牲代码的灵活性来换取垂直业务领域的研发效能提升。我主要负责自定义 React Hooks 的开发与可视化配置面板 UI 组件的设计。（React + Mobx + Typescript + Webpack）
+Low Code 低代码搭建是今年软件行业的潮流，也是公司今年主推的 SaaS 产品新特性，其本质是为了实现跨专业领域的赋能，解决前端研发资源投入不足的问题。我主要负责编辑器基座、智能网格画布、可视化配置面板的研发，后期转向物料素材中心的建设。截止至目前，已实现低代码物料素材流通数 500+，流转次数 2000+，降本约合 1300 人/天。(技术栈：Umi + Formily + AntdV)
 
 **<code>2017.07 ~ 2018.09</code>** 联和金融集团有限公司 / 开发工程师
 
-任职期间主要负责 Hey UI 组件库的开发与文档维护工作，因为在初级版本的 AntD v1.0 中无法实现黑夜模式，而且一些特殊的 UI 组件无法支撑 CNABS 资产证券化中后台业务的 UI 设计需求，我基于 Stroybook 重新封装 rc-components 实现了一套平行于 AntD 的企业级组件库。不仅满足了团队业务开发的需要，而且还使得公司的设计资产能够以技术的方式沉淀下来。
+任职期间主要负责 Hey UI 组件库的开发与文档维护工作。早期的 AntD v1.0 版本中无法实现黑夜模式，该组件库无法支撑 CNABS 资产证券化中后台业务特定场景下的 UI 设计需求，我基于 Stroybook 重新封装 rc-components 实现了一套平行于 AntD 的企业级组件库。不仅满足了团队业务开发的需要，而且还使得公司的设计资产能够以技术文档的方式沉淀下来。
 
 <!-- <br/>
 
